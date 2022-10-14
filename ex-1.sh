09e5633cb722b4ea4e47438b12e840155c3bc3b4
Fiziev Vagif <vagasuper560469@gmail.com>
2022-10-14 06:51:35 +0300

    ex-1 done

---
+++ ex-1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir projects
+cd projects
+mkdir my_project
+cd my_project
+mkdir directory1
+cd directory1
+mkdir subdirectory1
+cd ../
+mkdir directory2
+cd directory2
+touch hello.txt
+echo "Hello world!" > hello.txt
+cd ../
+mkdir directory3
+cd directory3
+mkdir subdirectory2
